19826b04bb36688b8e025295f487d8b919418fe1
Leigh Oliver <leighdoliver@gmail.com>
2020-08-25 20:08:00 +0100

    Added newrelic agent to parser

--- replaybot_parser/config/start.sh
+++ replaybot_parser/config/start.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 nginx
-uwsgi --lazy --ini /config/uwsgi.ini --py-autoreload 1
+newrelic-admin generate-config $NEWRELIC_LICENSE_KEY newrelic.ini
+sed -i 's/app_name\ =\ .*/app_name\ =\ replaybot_parser/' newrelic.ini
+echo "log_file = stderr" >> newrelic.ini
+NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program uwsgi --lazy --ini /config/uwsgi.ini --py-autoreload 1
