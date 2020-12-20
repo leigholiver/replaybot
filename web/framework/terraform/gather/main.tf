2034f9c2788ae1153ef6ba6d24a8b20d1a2cdf1c
Leigh Oliver <leigh@leigholiver.com>
2020-12-20 13:53:20 +0000

    how to wait for things in terraform?

--- web/framework/terraform/gather/main.tf
+++ web/framework/terraform/gather/main.tf
@@ -34,13 +34,13 @@ resource "null_resource" "package_deps" {
 resource "null_resource" "package_dirs" {
   count = length(local.dir_paths)
   triggers = {
+    pip_changed = null_resource.package_deps.id
     src_changed = module.hashes.hashes[count.index]
   }
 
   provisioner "local-exec" {
     command = "${local.dir_paths[count.index].basepath == "" ? "" : "mkdir -p ${var.build_dir}/${local.dir_paths[count.index].basepath} && "} cp -r ${local.dir_paths[count.index].dir}/* ${var.build_dir}/${local.dir_paths[count.index].basepath}"
   }
-  depends_on = [null_resource.package_deps]
 }
 
 # hack to make sure that everything is finished
@@ -50,12 +50,11 @@ data "null_data_source" "wait_for_packaging" {
     lambda_exporter_id = null_resource.package_dirs[0].id
     build_dir          = var.build_dir
   }
-  depends_on = [null_resource.package_dirs]
 }
 
 data "archive_file" "lambda_zip" {
   type        = "zip"
   source_dir  = data.null_data_source.wait_for_packaging.outputs["build_dir"]
   output_path = var.zip_path
-  depends_on = [null_resource.package_dirs, null_resource.package_deps]
+  depends_on = [null_resource.package_dirs[0].id, null_resource.package_deps.id]
 }
